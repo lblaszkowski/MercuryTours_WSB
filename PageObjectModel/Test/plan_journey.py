eb7572550c8701971f0e215a1e48aa73f8d95297
lblaszkowski <lblaszkowski@onet.pl>
2019-05-15 20:47:24 +0200

    Dodanie DDT do planu podróży i zmiana nazwy plików z polskiego na angielski

--- PageObjectModel/Test/plan_journey.py
+++ PageObjectModel/Test/plan_journey.py
@@ -4,9 +4,12 @@ import unittest
 from PageObjectModel.Pages.loginPage import LoginPages
 from PageObjectModel.Pages.planTravelsPage import PlanTravelsPage
 from PageObjectModel.Application.application import Application_page
+from PageObjectModel.Data_test.Function_for_downloading_test_data.test_data_loader import get_data
+from ddt import ddt, data, unpack
 
 
-class PlanPodrozy_Pages(unittest.TestCase):
+@ddt
+class Plan_Journey_Pages(unittest.TestCase):
 
     @classmethod
     def setUp(self):
@@ -16,12 +19,14 @@ class PlanPodrozy_Pages(unittest.TestCase):
     def tearDown(self):
         self.app.destroy()
 
-    def test_PlanPodrozy(self):
+    @data(*get_data("../Data_test/Data_test_pages/Data_test_plan_journey/data_test_plan_journey.csv"))
+    @unpack
+    def test_Plan_Journey(self, valid_email, valid_password):
         driver = self.app.driver
         login = LoginPages(driver)
         login.click_signOnbutton()
-        login.enter_username('marcelina.kos@interia.pl')
-        login.enter_password('Test123')
+        login.enter_username(valid_email)
+        login.enter_password(valid_password)
         login.click_login()
         plantravels = PlanTravelsPage(driver)
         plantravels.click_flights()
--- PageObjectModel/TestSuite/testsuite.py
+++ PageObjectModel/TestSuite/testsuite.py
@@ -1,13 +1,13 @@
 import unittest
 
 from PageObjectModel.Test.login import Login_Pages
-from PageObjectModel.Test.plan_podrozy import PlanPodrozy_Pages
+from PageObjectModel.Test.plan_journey import Plan_Journey_Pages
 from PageObjectModel.Test.rejestracja import NewtoursDemoautRegistration
 from PageObjectModel.Test.rezerwacja_lotu import NewtoursDemoautRegistrationTest
 
 def full_suite():
     test_suite = unittest.TestSuite()
-    test_suite.addTest(unittest.makeSuite(PlanPodrozy_Pages))
+    test_suite.addTest(unittest.makeSuite(Plan_Journey_Pages))
     test_suite.addTest(unittest.makeSuite(Login_Pages))
     test_suite.addTest(unittest.makeSuite(NewtoursDemoautRegistration))
     test_suite.addTest(unittest.makeSuite(NewtoursDemoautRegistrationTest))
