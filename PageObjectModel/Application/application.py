430716ba2d791ba4e930f03dc6808d4bbe3971bc
lblaszkowski <lblaszkowski@onet.pl>
2019-06-04 20:28:07 +0200

    Poprawka  uruchomienia testów z Mozilla lub Chrome

--- PageObjectModel/Application/application.py
+++ PageObjectModel/Application/application.py
@@ -1,30 +1,27 @@
 from selenium import webdriver
 import datetime
 
-browser = "Chrome"
-# browser = "Mozilla"
 
 
 class Application_page:
 
-    def __init__(self):
-        if browser == "Chrome":
+    def __init__(self, browser="chrome"):
+        if browser == "chrome":
             self.driver = webdriver.Chrome(executable_path=r'C:\driver_selenium\ChromeDrive_74\chromedriver.exe')
             self.driver.get('http://newtours.demoaut.com/')
             self.driver.maximize_window()
             print("-----------------")
             print("Run Started at :" + str(datetime.datetime.now()))
             print("start Chrome")
-        elif browser == "Mozilla":
-           self.driver = webdriver.Firefox(executable_path=r'../Drivers/FirefoxDrive_24/geckodriver.exe')
-           self.driver.get('http://newtours.demoaut.com/')
-           self.driver.maximize_window()
-           print("-----------------")
-           print("Run Started at :" + str(datetime.datetime.now()))
-           print("Start Mozilla")
+        elif browser == "mozilla":
+            self.driver = webdriver.Firefox(executable_path=r'../Drivers/FirefoxDrive_24/geckodriver.exe')
+            self.driver.get('http://newtours.demoaut.com/')
+            self.driver.maximize_window()
+            print("-----------------")
+            print("Run Started at :" + str(datetime.datetime.now()))
+            print("Start Mozilla")
         else:
-            print("bład")
-
+            print("Brak przeglądarki")
 
     def destroy(self):
         print("------------------")
