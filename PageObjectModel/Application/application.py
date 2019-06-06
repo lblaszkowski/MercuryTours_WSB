44612944e4469e801d5fc51553231511e27620ce
lblaszkowski <lblaszkowski@onet.pl>
2019-06-06 20:19:18 +0200

    Poprawka  pliku application

--- PageObjectModel/Application/application.py
+++ PageObjectModel/Application/application.py
@@ -1,19 +1,18 @@
 import datetime
 from PageObjectModel.Application.Browser_pages.browser import Browser_pages_url
 
-
 class Application_page:
 
     def __init__(self):
         self.bro = Browser_pages_url()
-        self.driver.get('http://newtours.demoaut.com/')
-        self.driver.maximize_window()
+        self.bro.browser_url_pages()
+        self.bro.driver.get('http://newtours.demoaut.com/')
+        self.bro.driver.maximize_window()
         print("-----------------")
         print("Run Started at :" + str(datetime.datetime.now()))
-
 
     def destroy(self):
         print("------------------")
         print("Run Completed at :" + str(datetime.datetime.now()))
-        self.driver.close()
-        self.driver.quit()
+        self.bro.driver.close()
+        self.bro.driver.quit()
--- PageObjectModel/TestSuite/testsuite.py
+++ PageObjectModel/TestSuite/testsuite.py
@@ -2,16 +2,16 @@ import unittest
 
 
 from PageObjectModel.Test.login import Login_Pages
-from PageObjectModel.Test.plan_journey import Plan_Journey_Pages
-from PageObjectModel.Test.registration import Registration_Pages
-from PageObjectModel.Test.booking_flight import Booking_Flight_Pages
+# from PageObjectModel.Test.plan_journey import Plan_Journey_Pages
+# from PageObjectModel.Test.registration import Registration_Pages
+# from PageObjectModel.Test.booking_flight import Booking_Flight_Pages
 
 def full_suite():
     test_suite = unittest.TestSuite()
-    test_suite.addTest(unittest.makeSuite(Plan_Journey_Pages))
+    # test_suite.addTest(unittest.makeSuite(Plan_Journey_Pages))
     test_suite.addTest(unittest.makeSuite(Login_Pages))
-    test_suite.addTest(unittest.makeSuite(Registration_Pages))
-    test_suite.addTest(unittest.makeSuite(Booking_Flight_Pages))
+    # test_suite.addTest(unittest.makeSuite(Registration_Pages))
+    # test_suite.addTest(unittest.makeSuite(Booking_Flight_Pages))
     return test_suite
 
 if __name__ == '__main__':
